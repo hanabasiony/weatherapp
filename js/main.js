c6afb76132be81467b8a46f9b54f1c517649bbc4
hana <omarfotoh9@gmail.comhttps://github.com/hanabasiony/bookmark.gitgit add .git commit -m initial>
2024-12-08 00:28:01 +0200

    sec commit

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ fetch('https://api.ipify.org?format=json')
 
 function locApi(locationIP) {
     var loc = new XMLHttpRequest()
-    loc.open('GET', `https://apiip.net/api/check?ip=${locationIP}&accessKey=6860db4e-ab53-4930-9695-2b684629c567`)
+    loc.open('GET', `https://apiip.net/api/check?ip=${locationIP}&accessKey=cb6c1c43-44e8-43cd-99ea-84b7870513cf`)
     loc.send()
     loc.addEventListener('readystatechange', function () {
         if (loc.readyState === 4 && loc.status >= 200 && loc.status < 300) {
@@ -30,7 +30,7 @@ function locApi(locationIP) {
 
 function reqFirstOpenWeather() {
     var req2 = new XMLHttpRequest();
-    req2.open('GET', `https://api.weatherapi.com/v1/forecast.json?key=4fe14684ed8f4d4faee195745240612&q=${locArr.countryName}&days=3`)
+    req2.open('GET', `https://api.weatherapi.com/v1/forecast.json?key=5fc99c2a45c347e89a5213819240712&q=${locArr.countryName}&days=3`)
     req2.send()
     req2.addEventListener('readystatechange', function () {
         if (req2.readyState === 4 && req2.status >= 200 && req2.status < 300) {
@@ -49,10 +49,13 @@ function reqFirstOpenWeather() {
 
 var inputSearch = document.querySelector('.input-search')
 var inputSearchValue = ''
-inputSearch.addEventListener('keyup', function () {
-    inputSearchValue = inputSearch.value
+
+
+inputSearch.addEventListener('change', function (){
     var req = new XMLHttpRequest();
-    req.open('GET', `https://api.weatherapi.com/v1/forecast.json?key=4fe14684ed8f4d4faee195745240612&q=${inputSearchValue}&days=3`)
+    inputSearchValue = inputSearch.value
+    console.log(inputSearchValue);
+    req.open('GET', `https://api.weatherapi.com/v1/forecast.json?key=5fc99c2a45c347e89a5213819240712&q=${inputSearchValue}&days=3`)
     req.send()
     req.addEventListener('readystatechange', function () {
         if (req.readyState === 4 && req.status >= 200 && req.status < 300) {
@@ -73,92 +76,132 @@ inputSearch.addEventListener('keyup', function () {
 function displayCard1() {
     console.log(arr)
     var cartona1 = ''
+
     cartona1 = `
-          <div class="inner m-2">
-            <div class="location">
-              <h3>${arr.location.name}</h3>
-            </div>
+    <div class="location">
+                  <h5>
+                  ${arr.location.name}
+                  </h5>
+                </div>
+                <div class="deg">
+                  ${arr.forecast.forecastday[0].day.maxtemp_c}<span>°C</span>
+                </div>
 
-            <div class="deg">
-              ${arr.forecast.forecastday[0].day.maxtemp_c}<span>°C</span>
-            </div>
+                <div class="forcast-icon">
+                  <img src="https:${arr.forecast.forecastday[0].day.condition.icon}" class="">
+                </div>
 
-            <div class="forcast-icon">
-              <img src="	https:${arr.forecast.forecastday[0].day.condition.icon}" class="">
-            </div>
+                <div class="staus">
+                ${arr.forecast.forecastday[0].day.condition.text}
+                </div>
 
-            <div class="staus">
-            ${arr.forecast.forecastday[0].day.condition.text}
-            </div>
-            <div class="wind-box d-flex justify-content-start gap-4 align-items-center mt-5">
-              <div class="cont d-flex justify-content-center align-items-center">
-                <img src="./img/icon-umberella.png" alt="">
-                <p class="rain ms-1">20%</p>
-              </div>
-              <div class="cont d-flex justify-content-center align-items-center">
-                <img src="./img/icon-wind.png" alt="">
-                <p class="wind  ms-1">${arr.forecast.forecastday[0].day.maxwind_kph}km</p>
-              </div>
-              <div class="cont d-flex justify-content-center align-items-center">
-                <img src="./img//icon-compass.png" alt="">
-                <p class="compas  ms-1">east</p>
-              </div>
+                <div class="wind-box d-flex justify-content-start gap-4 align-items-center mt-5">
+                  <div class="cont d-flex justify-content-center align-items-center">
+                    <img src="./img/icon-umberella.png" alt="">
+                    <p class="rain ms-1">20%</p>
+                  </div>
+                  <div class="cont d-flex justify-content-center align-items-center">
+                    <img src="./img/icon-wind.png" alt="">
+                    <p class="wind  ms-1">${arr.forecast.forecastday[0].day.maxwind_kph}km</p>
+                  </div>
+                  <div class="cont d-flex justify-content-center align-items-center">
+                    <img src="./img//icon-compass.png" alt="">
+                    <p class="compas  ms-1">east</p>
+                  </div>
 
-            </div>
-          </div>
-        `
+                </div>
 
-    document.querySelector('.card1').innerHTML = cartona1
+    `
+
+    document.querySelector('.body-1 .inner').innerHTML = cartona1
 }
 function displayCard2() {
     console.log(arr)
     var cartona2 = ''
+    // cartona2 = `
+    //     <div class="inner d-flex flex-column justify-content-center align-items-center w-100 mt-5">
+    //         <div class="img-cont">
+    //           <img src="https:${arr.forecast.forecastday[1].day.condition.icon}" alt="">
+    //         </div>
+
+    //         <p class="big-degree">
+    //           ${arr.forecast.forecastday[1].day.maxtemp_c}<span>°C</span>
+    //         </p>
+
+    //         <p class="small-degree">
+    //           ${arr.forecast.forecastday[1].day.mintemp_c}<span>°C</span>
+    //         </p>
+
+    //         <p class="status">
+    //         ${arr.forecast.forecastday[1].day.condition.text}
+    //         </p>
+    //       </div>
+    //     `
     cartona2 = `
-        <div class="inner d-flex flex-column justify-content-center align-items-center w-100 mt-5">
-            <div class="img-cont">
-              <img src="https:${arr.forecast.forecastday[1].day.condition.icon}" alt="">
-            </div>
+        <div class="inner d-flex flex-column justify-content-center align-items-center w-100 ">
+                <div class="img-cont">
+                  <img src="https:${arr.forecast.forecastday[1].day.condition.icon}" alt="">
+                </div>
 
-            <p class="big-degree">
-              ${arr.forecast.forecastday[1].day.maxtemp_c}<span>°C</span>
-            </p>
+                <p class="big-degree">
+                  ${arr.forecast.forecastday[1].day.maxtemp_c}<span>°C</span>
+                </p>
 
-            <p class="small-degree">
-              ${arr.forecast.forecastday[1].day.mintemp_c}<span>°C</span>
-            </p>
+                <p class="small-degree">
+                 ${arr.forecast.forecastday[1].day.mintemp_c}<span>°C</span>
+                </p>
 
-            <p class="status">
-            ${arr.forecast.forecastday[1].day.condition.text}
-            </p>
-          </div>
+                <p class="status">
+                ${arr.forecast.forecastday[1].day.condition.text}
+                </p>
+              </div>
         `
 
-    document.querySelector('.card2').innerHTML = cartona2
+    document.querySelector('.body-2').innerHTML = cartona2
 }
 function displayCard3() {
     console.log(arr)
     var cartona3 = ''
-    cartona3 = `
-        <div class="inner d-flex flex-column justify-content-center align-items-center w-100 mt-5">
-            <div class="img-cont">
-              <img src="https:${arr.forecast.forecastday[2].day.condition.icon}" alt="">
-            </div>
+    // cartona3 = `
+    //     <div class="inner d-flex flex-column justify-content-center align-items-center w-100 mt-5">
+    //         <div class="img-cont">
+    //           <img src="https:${arr.forecast.forecastday[2].day.condition.icon}" alt="">
+    //         </div>
 
-            <p class="big-degree">
-              ${arr.forecast.forecastday[2].day.maxtemp_c}<span>°C</span>
-            </p>
+    //         <p class="big-degree">
+    //           ${arr.forecast.forecastday[2].day.maxtemp_c}<span>°C</span>
+    //         </p>
 
-            <p class="small-degree">
-              ${arr.forecast.forecastday[2].day.mintemp_c}<span>°C</span>
-            </p>
+    //         <p class="small-degree">
+    //           ${arr.forecast.forecastday[2].day.mintemp_c}<span>°C</span>
+    //         </p>
 
-            <p class="status">
-            ${arr.forecast.forecastday[2].day.condition.text}
-            </p>
-          </div>
+    //         <p class="status">
+    //         ${arr.forecast.forecastday[2].day.condition.text}
+    //         </p>
+    //       </div>
+    //     `
+        cartona3 =`
+        <div class="inner d-flex flex-column justify-content-center align-items-center w-100 ">
+                <div class="img-cont">
+                  <img src="${arr.forecast.forecastday[2].day.condition.icon}" alt="">
+                </div>
+
+                <p class="big-degree">
+                 ${arr.forecast.forecastday[2].day.maxtemp_c}<span>°C</span>
+                </p>
+
+                <p class="small-degree">
+                  ${arr.forecast.forecastday[2].day.mintemp_c}<span>°C</span>
+                </p>
+
+                <p class="status">
+                ${arr.forecast.forecastday[2].day.condition.text}
+                </p>
+              </div>
         `
 
-    document.querySelector('.card3').innerHTML = cartona3
+    document.querySelector('.body-3').innerHTML = cartona3
 }
 function diplayHeader1() {
     const dateString = arr.forecast.forecastday[0].date
